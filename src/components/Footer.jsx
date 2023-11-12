9657a886f9ad63248c83b2a47c4d64ceeeb4f72b
betinaferrao <betina.ferrao20002@gmail.com>
2023-11-12 10:55:21 -0300

    footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
 import shared from '../styles/Shared.module.css'
 import styles from '../styles/Footer.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer () {
   return (
     <div className={`${styles.fter} ${shared.flex} ${shared.row} ${shared.alignCenter}`}>
       <p>&copy; 2023 Estágios INE. Todos os direitos reservados</p>
-      <p>links</p>
+      <p className={`${shared.flex} ${shared.row} ${shared.alignCenter} ${styles.social_links}`}>
+        <a href="https://www.linkedin.com/feed/"><FontAwesomeIcon icon={faLinkedinIn} style={{ color: "#fff" }} /></a>
+        <a href="https://github.com/rlneto/sinestagios"><FontAwesomeIcon icon={faGithub} style={{ color: "#fff" }} /></a>
+      </p>
+     {/* <p><img className={`${styles.social_links}`} src='linkedin-in.svg'></img>links</p> */} 
     </div>
   )
 }
