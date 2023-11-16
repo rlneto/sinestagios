1e6398caf6e520dc66c5e0fc2757d6c4a5e22107
betinaferrao <betina.ferrao20002@gmail.com>
2023-11-16 00:22:49 -0300

    cards

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -2,28 +2,31 @@ import Card from '../components/Card'
 import { useContext } from 'react'
 import UserContext from '../UserContext'
 import shared from '../styles/Shared.module.css'
+import styles from '../styles/Inbox.module.css'
 
 export default function Inbox () {
   const { user } = useContext(UserContext)
 
   return (
-    <div className={`${shared.flex} ${shared.column}`}>
-      <div className={`${shared.row} ${shared.marginBottom} ${shared.textAlignCenter}`}>
-      <h1>Caixa de Entrada de {user.nome}</h1>
+    <div className={`${shared.flex} ${shared.column} ${shared.marginTop}`}>
+      <div className={`${shared.row} ${shared.marginBottom} ${shared.textAlignCenter} ${shared.textSize12}`}>
+        <h1>Caixa de Entrada de <span className={`${shared.colorBlue}`}>{user.nome}</span></h1>
       </div>
       <div className={`${shared.row} ${shared.marginBottom} ${shared.textAlignCenter}`}>
-      <p>Email: {user.email}</p></div>
+        <p>Email: {user.email}</p></div>
       <div className={`${shared.row} ${shared.marginBottom} ${shared.textAlignCenter}`}>
-      <h3>Mensagens na Caixa de Entrada</h3>
+        <h3>Mensagens na Caixa de Entrada</h3>
       </div>
     
       <ul>
         {user.inbox.map((message, index) => (
           <li className={`${shared.marginBottom}`} key={index}>
-            <Card>
-            <strong>De:</strong> {message.remetente}<br />
-            <strong>Título:</strong> {message.titulo}<br />
-            <strong>Mensagem:</strong> {message.mensagem}
+          <Card>
+            <div className={`${shared.flex} ${shared.column} ${shared.bigGap}`}>
+              <div><strong>De:</strong> {message.remetente}</div>
+              <div><strong>Título:</strong> {message.titulo}</div>
+              <div><strong>Mensagem:</strong> {message.mensagem}</div>
+            </div>
           </Card>
           </li>
         ))}
@@ -35,10 +38,10 @@ export default function Inbox () {
         {user.outbox.map((message, index) => (
           <li className={`${shared.marginBottom}`} key={index}>
             <Card>
-            <strong>Para:</strong> {message.destinatario}<br />
-            <strong>Título:</strong> {message.titulo}<br />
-            <strong>Mensagem:</strong> {message.mensagem}
-          </Card>
+              <strong>Para:</strong> {message.destinatario}<br />
+              <strong>Título:</strong> {message.titulo}<br />
+              <strong>Mensagem:</strong> {message.mensagem}
+            </Card>
           </li>
         ))}
         <p></p>
