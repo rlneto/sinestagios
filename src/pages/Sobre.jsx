2a633a5bcd4bfbc60a70507818efb4f8ceda23ab
Rafaela Borges <rafasborgesw@gmail.com>
2023-11-23 23:22:47 -0300

    css

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,6 +1,14 @@
+import shared from '../styles/Shared.module.css'
+import styles from '../styles/Sobre.module.css'
 
 export default function Sobre() {
     return (
-        <h1>Ola</h1>
+        <div className={`${shared.flex} ${shared.column} ${shared.alignCenter}`}>
+            <div className={`${shared.row} ${shared.marginBottom} ${shared.textAlignCenter}`}>
+                <h1 className={`${styles.title}`}>Vagas de estágio</h1>
+            </div>
+            <p className= {`${styles.body}`}> texto</p>
+            <div><img src='/Users/rafaelaborges/Desktop/sinestagios/public/ufsc-20felipe-20carneiro_5_3.png' alt="Imagem UFSC"/> </div>
+        </div>
     )
 }
