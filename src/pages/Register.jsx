825d027b4001ddecc902d8cb885f4e38e9300de6
rlneto <rlneto@live.com>
2023-11-26 18:43:22 -0300

    Login e cadastro na api v0

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,9 @@
 import { Link, useNavigate } from 'react-router-dom'
-import { useRef } from 'react'
-import { users } from '../db/Database'
-import Button from '../components/Button'
+import { useRef, useState } from 'react'
 import InputText from '../components/InputText'
+import { Button, CircularProgress } from '@mui/material'
 import Card from '../components/Card'
+import axios from 'axios'
 import shared from '../styles/Shared.module.css'
 import styles from '../styles/Register.module.css'
 
@@ -13,32 +13,49 @@ export default function Register () {
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
   const navegar = useNavigate()
-  const RegisterHandler = () => {
+  const [loading, setLoading] = useState(false)
+  const api = axios.create({ baseURL:'https://estagios-ine-api.onrender.com/api/v1/auth/cadastro'})
+  const RegisterHandler = async () => {
+    setLoading(true)
     if (passwordRef.current.value === confirmPasswordRef.current.value) {
       const user = {
         nome: nameRef.current.value,
         email: emailRef.current.value,
         password: passwordRef.current.value
       }
-      users.push(user)
-      alert('Usuário cadastrado com sucesso!')
-      navegar('/login')
-  } else {
-    alert('As senhas não conferem')
-  } }
+      try {
+        const response = await api.post('', user)
+        if (response.status === 201) {
+          alert('Usuário cadastrado com sucesso!')
+          setLoading(false)
+          navegar('/login')
+        } else {
+          alert('Erro ao cadastrar usuário')
+          setLoading(false)
+          navegar('/register')
+        }
+      } catch (error) {
+        alert('Erro ao cadastrar usuário')
+        setLoading(false)
+        navegar('/register')
+      }
+    } else {
+      alert('As senhas não conferem')
+    }
+  }
 
   return (
     <div className={`${shared.flex} ${shared.column} ${shared.alignCenter} ${shared.marginTop}`}>
       <Card>
       <div className={`${styles.title}`}><h1>Cadastro</h1></div>
       <div>
-        <form className={`${styles.box_form}`}>
+        <form className={`${styles.box_form} ${shared.marginBottom}`}>
           <InputText tipo='text' nome='name' rotulo='Nome' referencia={nameRef} />
           <InputText tipo='email' nome='email' rotulo='E-mail' referencia={emailRef} />
           <InputText tipo='password' nome='password' rotulo='Senha' referencia={passwordRef} />
           <InputText tipo='password' nome='confirmPassword' rotulo='Confirme a senha' referencia={confirmPasswordRef} />
           </form>
-          <div className={`${styles.btn}`}><Button text={`Registrar-se`} action={RegisterHandler} estilo={shared.btnLogin}>Cadastrar</Button></div>
+          <Button fullWidth onClick={RegisterHandler} variant="contained" color="primary" className={`${styles.btn}`} disabled={loading}>{loading ? <CircularProgress size={24} /> : 'Entrar'}</Button>
         </div>
         <div className={`${shared.marginTop} ${shared.textAlignCenter}`}>
           <span>Já possui cadastro?</span>
