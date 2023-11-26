d9feb0c3ecffcb57fd90f63cfb5ce78013dc44c1
rlneto <rlneto@live.com>
2023-11-26 16:08:39 -0300

    login axios

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,38 +1,40 @@
 import { useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import LoggedInContext from '../LoggedInContext'
 import UserContext from '../UserContext'
-import { users } from '../db/Database.js'
 import InputText from '../components/InputText'
 import Card from '../components/Card'
-import Button from '../components/Button'
+import { Button, CircularProgress } from '@mui/material'
 import shared from '../styles/Shared.module.css'
 import styles from '../styles/Login.module.css'
+import axios from 'axios'
 
 export default function Login () {
   const { setLoggado } = useContext(LoggedInContext)
   const { setUser } = useContext(UserContext)
   const navegar = useNavigate();
-  
-  const LoginHandler = () => {
+  const [loading, setLoading] = useState(false)
+  const api = axios.create({
+    baseURL: 'https://estagios-ine-api.onrender.com/api/v1/auth/login'
+  })
+  const LoginHandler = async () => {
+    setLoading(true)
     const email = emailRef.current.value
     const password = passwordRef.current.value
     try {
-      const user = users.find(user => user.email === email)
-      if (user) {
-        if (user.password === password) {
-          setUser(user)
-          setLoggado(true)
-          console.log(user)
-          navegar('/dashboard/all')
-        } else {
-          alert('Senha incorreta')
-          navegar('/login')
-        }
-      }
+      const response = await api.post('', {
+        email: email,
+        password: password
+      })
+      const user = response.data.user
+      setUser(user)
+      setLoggado(true)
+      setLoading(false)
+      navegar('/dashboard/all')
     } catch (error) {
       alert('Usuário não encontrado')
+      setLoading(false)
       navegar('/login')
     }
   }
@@ -43,13 +45,13 @@ export default function Login () {
       <div className={`${shared.row} ${shared.marginBottom}`}>
       <Card>
         <div className={`${styles.title}`}><h1>Login</h1></div>
-        <div className={`${shared.flex} ${shared.column}`}>
+        <div className={`${shared.flex} ${shared.column} ${shared.marginBottom}`}>
           <form className={`${styles.box_form}`}>
             <InputText tipo='email' nome='email' rotulo='E-mail' valorPadrao={'pogarrido@earth.com.br'} referencia={emailRef} />
           <InputText tipo='password' nome='password' rotulo='Senha' referencia={passwordRef} valorPadrao={'quantacoisaboa'}/>
           </form>
         </div>
-        <div className={`${styles.btn}`}><Button action={LoginHandler} text={`Entrar`} estilo={shared.btnLogin} /></div>
+        <Button fullWidth onClick={LoginHandler} variant="contained" color="primary" className={`${styles.btn}`} disabled={loading}>{loading ? <CircularProgress size={24} /> : 'Entrar'}</Button>
         <div className={`${shared.flex} ${shared.row} ${shared.justifyCenter} ${styles.box_cadastro}`}>
           <p>Não tem uma conta?</p>
           <Link to='/register' className={`${styles.nav_link}`}>Cadastrar-se</Link>
